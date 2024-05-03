6bd9553e78d040b407c892ebfc8ba1d03fcaa2ef
christianamaury <chris.amaury@hotmail.com>
2024-05-02 17:41:21 -0700

    Base Default Project

---
+++ Components/MaxWidthWrapper.tsx
@@ -0,0 +1,19 @@
+import {ReactNode} from "react"
+
+//Reusable for All Pages: Left & Right Side of our Pages;
+//It would take props;
+const MaxWidthWrapper = ({
+    className, 
+    children
+}:{
+    className?: string
+    children: ReactNode
+
+}) => {
+
+    <div className="mx-auto w-full"> 
+    
+     </div>
+}
+
+export default MaxWidthWrapper
