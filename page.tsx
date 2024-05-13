267f489860ae9d5d17d45e2c474e7d7b17ae4796
christianamaury <chris.amaury@hotmail.com>
2024-05-12 17:33:19 -0700

    Adding link section to a new tab page

--- page.tsx
+++ page.tsx
@@ -10,8 +10,19 @@ export default function Home() {
           <p className="text-sm font-semibold text-gray-700">
               DocuLeer ya esta disponible.
           </p>
-
       </div>
+      <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+              Chatea con tu <span className='text-red-600'> Documento PDF </span> en tan solo segundos.
+      </h1>
+
+      <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+        DocuLeer te permitira tener cualquier tipo de conversación con tu documento de PDF. Solo asegurate de subir tu documento y comienza a hacer cualquier tipo de pregunta relacionada al documento. 
+
+      </p>
+
+      <Link href='/dashboard' target='_blank'>
+        Empieza Ahora
+      </Link>
 
     </MaxWidthWrapper>
   )
