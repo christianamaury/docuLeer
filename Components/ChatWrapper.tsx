67feb7930cb3cea272e3e65168145fc0680d8fc1
christianamaury <chris.amaury@hotmail.com>
2024-07-07 02:01:19 -0700

    Updating the ChatWrapper Component

--- Components/ChatWrapper.tsx
+++ Components/ChatWrapper.tsx
@@ -1,5 +1,11 @@
+
+//Handle all loading logic; 
+
 const ChatWrapper = () => {
-    return <div> Easy </div>
+    return <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'> 
+            
+    
+     </div>
 }
 
 export default ChatWrapper
