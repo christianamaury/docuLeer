3c9f70a0a77c559d8b2b80719841df7af6cdca95
christianamaury <chris.amaury@hotmail.com>
2024-07-13 11:08:57 -0700

    Updating ChatContext Component

--- Components/chat/ChatContext.tsx
+++ Components/chat/ChatContext.tsx
@@ -1,4 +1,78 @@
 //This would consume our components; 
-import {createContext} from "react";
+import {ReactNode, createContext, useState, useEffect} from "react";
+//Destructive Notifications, Toast, shadcn/ui library; 
+import { useToast } from "@/components/ui/use-toast"
+import { useMutation } from "@tanstack/react-query";
 
-export const ChatContext = createContext({})
+//Type of what we want the ChatContext to be; 
+type StreamResponse = {
+
+       //Function to add a Message; returns void
+       addMessage: () => void,
+       message: string,
+       handleInputChange: (event: React.ChangeEvent <HTMLTextAreaElement>) => void, 
+       isLoading: boolean, 
+} 
+
+export const ChatContext = createContext<StreamResponse>({
+
+    //Function to add a Message; 
+    addMessage: () => {},
+    message:'',
+    handleInputChange: () => {}, 
+    isLoading: false, 
+
+})
+
+//fileId and Child props
+//ReactNode represents everything that can be rendered
+interface Props {
+    fileId: string,
+    children: ReactNode
+}
+
+//Wrapping interface Props on the Component; 
+export const ChatContextProvider = ({fileId, children}: Props) => {
+
+const [message, setMessage] = useState<string>('')
+const {toast} = useToast();
+//API route;
+//Destructing the Function; 
+const {mutate: sendMessage} = useMutation({
+
+    mutationFn: async ({message}: {message: string}) => {
+        const response = await fetch('/api/message', {
+            method: 'POST', 
+            body: JSON.stringify({
+                fileId, 
+                message, 
+            }),
+        })
+        if(!response.ok){
+            throw new Error ("Hubo un error mandando el mensaje")
+        }
+
+        return response.body
+
+    },
+})
+
+//Handling input Changes; 
+//It receives an event; 
+const handleInputChange
+
+const addMessage = () => sendMessage({message})
+
+    return (
+        <ChatContext.Provider value={{
+                addMessage, 
+                message, 
+                handleInputChange,
+                isLoading, 
+        }}>
+
+            {children}
+        </ChatContext.Provider>
+    )
+ 
+}
