f6c12eca92cf5cf09e2ee13ec8b98c0e92cedec7
christianamaury <chris.amaury@hotmail.com>
2024-07-21 21:18:21 -0700

    Adding useRef method to the button class for the chatInput component

--- Components/chat/ChatInput.tsx
+++ Components/chat/ChatInput.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 import {Textarea} from "@/components/ui/textarea"
 //Adding Library for icons; 
 import { Ghost, MessageSquare, Plus, Trash, Loader2, Send } from 'lucide-react'
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import { ChatContext } from "./ChatContext"
 
 //It takes a props property
@@ -19,25 +19,40 @@ const ChatInput = ({isDisabled}: ChatInputProps) => {
 //Accepts a Context Object and returns the current Context Value;
 const {addMessage, handleInputChange, isLoading, message, } = useContext (ChatContext)
 
+    //By default ref would be null;
+    //useRef returns a mutable object
+    //HTMLTextareaElement for manipulating the layout and presentantion of objects;
+    const textareaRef = useRef<HTMLTextAreaElement>(null)
+
     return (
     <div className='absolute bottom-0 left-0 w-full'>
         <form className='mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl'>
             <div className='relative flex h-full flex-1 items-stretch md:flex-col'> 
                 <div className='relative flex flex-col w-full flex-grow p-4'>
                     <div className='relative'>
-                        <Textarea rows={1} maxRows={4} autoFocus onKeyDown={(e) => {
+                        <Textarea rows={1} ref={textareaRef} maxRows={4} autoFocus onChange={handleInputChange} value={message} onKeyDown={(e) => {
                          if(e.key === "Enter" && !e.shiftKey){
                             e.preventDefault()
 
                             addMessage()
+
+                            //It could be null? 
+                            textareaRef.current?.focus()
                          }
                             
                         }} placeholder='Ingresa tu pregunta..' className='resize-none pr-12 text-base py-3 scrollbar-thumb-red scrollbar-thumb-rounded scrollbar-track-red-lighter scrollbar-w-2 scrolling-touch' /> 
                    
-                         <Button className='absolute bottom-1.5 right-[8px]' aria-label = 'send message'> 
+                        {/* Button should be disabled if we're loading or getting data from our API*/}
+                         <Button disabled={isLoading || isDisabled } className='absolute bottom-1.5 right-[8px]' aria-label = 'send message' type='submit' onClick={
+                            () => {
+
+                                addMessage()
+                                //It could be null;
+                                textareaRef.current?.focus
+                            }
+                         }> 
                            <Send className='h-4 w-4'/>
                          </Button>
-                   
                     </div>
                 </div>
             </div>
