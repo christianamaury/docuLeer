bcef287961873eb30f7efa8a0b4feb1d8b42fa06
christianamaury <chris.amaury@hotmail.com>
2025-02-14 20:10:11 -0700

    Fixing issues with the middleware.ts class

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,9 @@
+import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server"
 
 //Matcher is enforce by Next.JS. Only users can visit these pages. Protected;
+//Only login user can access these pages;
 export const config = {
-    matcher: ["/dashboard/:path*", "/auth-callback"]
+    matcher: ['/dashboard/:path*', '/auth-callback'],
 }
+
+export default authMiddleware
