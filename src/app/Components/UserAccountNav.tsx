3ea9cb331991375b39fe9e36d461b7b378ef5b81
christianamaury <chris.amaury@hotmail.com>
2025-02-18 21:22:35 -0700

    Adding Dropdown menu item to the user

--- src/app/Components/UserAccountNav.tsx
+++ src/app/Components/UserAccountNav.tsx
@@ -1,10 +1,13 @@
 //This gonna be a drop-down for when the user logs in.. 
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { getUserSubscriptionPlan } from "../lib/stripe"
 import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import Image from "next/image"
 import { Icons } from "./Icons"
+import Link from "next/link"
+import { Gem } from "lucide-react"
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components"
 
 //Passing props, type userAccountNav
 interface UserAccountNavProps {
@@ -44,6 +47,52 @@ const UserAccountNav = async ({email, imageUrl, name}: UserAccountNavProps) => {
                     </Avatar>
                 </Button>
             </DropdownMenuTrigger>
+
+           
+            <DropdownMenuContent className='bg-white' align='end'>
+                        <div className='flex items-center justify-start gap-2 p-2'>
+                            <div className='flex flex-col space-y-0.5 leading-none'>
+                                {/* If we have a name here, we would be rending */}
+                                {name && <p className='font-medium text-sm text-black'> {name} </p>}
+                                
+                                {/* Checking if we have an email..  */}
+                                {email && (
+                                <p className='w-[200px] truncate text-xs text-zinc-700'>
+                                    {email}
+                                </p>
+                                
+                                )}
+
+                            </div>
+
+                        </div>
+
+                        <DropdownMenuSeparator />
+
+                        <DropdownMenuItem asChild>
+                            <Link href='/dashboard'> Dashboard </Link>
+
+                        </DropdownMenuItem>
+
+                        <DropdownMenuItem asChild>
+                            {/* For the actual content we're doing a Conditional Check;  */}
+                            {susbcriptionPlan?.isSubscribed ? (
+                                <Link href='/dashboard/billing'> Maneja tu cuenta </Link>
+                            ): (
+                                <Link href='/pricing'> Plan Pro <Gem className='text-red-600 h-4 w-4 ml-1.5'/> </Link>
+                            )}
+
+                        </DropdownMenuItem>
+
+                        <DropdownMenuSeparator />
+
+                        <DropdownMenuItem className='cursor-pointer'>
+                            {/* We this method from Kinde Session*/}
+                             <LogoutLink> Cerrar Sesión </LogoutLink>
+
+                        </DropdownMenuItem>
+
+            </DropdownMenuContent>
 
         </DropdownMenu>
     )
