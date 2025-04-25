9640520b6301b002a2168bfe28015d5697b466b2
christianamaury <chris.amaury@hotmail.com>
2025-04-25 16:17:18 -0700

    Updating the auth folder structure and route.ts file

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,18 +1,7 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-//In order to be able to use it on the request parameters;
-//Returning a response Object;
-import {NextRequest, NextResponse} from "next/server";
+export const GET = handleAuth();
+export const POST = handleAuth();
 
-export async function GET (request: NextRequest, {params}: any ) {
-const endpoint = params.kindeAuth;
 
-//Ensuring 'handleAuhth' correctly returns a Response; 
-const response = await handleAuth(request, endpoint);
-
-return NextResponse.json(response);
-
-// return handleAuth(request, endpoint);
-
-}
 
